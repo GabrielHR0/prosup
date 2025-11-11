3052de590741eb2749bbca2c46716a0244d4e0c1
Jales <55924744+GabrielHR0@users.noreply.github.com>
2025-11-11 19:36:14 -0300

    feat: tabela senior

--- src/main/java/com/example/tabelas_classificacao/TabelaSeniorTest.java
+++ src/main/java/com/example/tabelas_classificacao/TabelaSeniorTest.java
@@ -1,5 +1,44 @@
 package com.example.tabelas_classificacao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.Biometria;
+import com.example.testes.Teste;
+
 public class TabelaSeniorTest extends Composite{
-    
+
+    @Override
+    public Leaf classificar(Double resultado, Teste teste, Biometria biometria) {
+        for (Component child : children) {
+            Leaf classificacao = child.classificar(resultado, teste, biometria);
+            if (classificacao != null) {
+                return classificacao;
+            }
+        }
+        return null;
+    }
+
+    public List<Leaf> classificacoes(Double resultado, Class<? extends Component> tipoAlvo, Teste teste, Biometria biometria) {
+            List<Leaf> leafs = new ArrayList<>();
+
+            for (Component child : children) {
+                
+                if (tipoAlvo.isInstance(child)) {
+                    if (child instanceof Composite compositeAlvo) {
+                        for (Component subChild : compositeAlvo.children) {
+                            if (subChild != null) {
+                                Leaf classificacao = subChild.classificar(resultado, teste, biometria);
+                                if (classificacao != null) {
+                                    leafs.add(classificacao);
+                                }
+                            }
+                        }
+                    } else if (child instanceof Composite){
+                        child.classificar(resultado, tipoAlvo, teste, biometria);
+                    }
+                }
+            }
+        return null;
+    }
 }
